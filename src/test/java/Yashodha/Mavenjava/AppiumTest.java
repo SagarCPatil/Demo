3ed74654d0ede70b528cdf147e72c9e569a52c83
Sagar <patils23293@gmail.com>
2021-01-26 15:06:13 +0530

    First commit

---
+++ src/test/java/Yashodha/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package Yashodha.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeappAndroid() {
+		System.out.println("nativeappAndroid");
+	}
+	
+	@Test
+	public void IOSapp() {
+		System.out.println("IOSapp");
+	}
+}
---
+++ src/test/java/Yashodha/Mavenjava/RestAPITest.java
@@ -0,0 +1,18 @@
+package Yashodha.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	
+	@Test
+	public void postJIRA() {
+		System.out.println("postJIRA");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+
+}
